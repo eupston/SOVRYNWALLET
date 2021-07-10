ac7789fa222b0703dc9a553cf9ca6c36d350a344
eupston <eupston130@hotmail.com>
2021-07-10 12:43:25 -0600

    fixed styling on mobile dark light button

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -26,21 +26,6 @@ function Footer() {
     <footer className="footer">
       <Container fluid>
         <Nav>
-          <NavItem>
-            <NavLink href="https://www.creative-tim.com/?ref=bdr-user-archive-footer">
-              Creative Tim
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://www.creative-tim.com/presentation?ref=bdr-user-archive-footer">
-              About Us
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="https://www.creative-tim.com/blog?ref=bdr-user-archive-footer">
-              Blog
-            </NavLink>
-          </NavItem>
         </Nav>
       </Container>
     </footer>
